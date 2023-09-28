e24e62f2c4fcfb149f3f6dd56652382c2516703d
Rlr <rlr>
2023-09-28 10:00:45 +0800

    sort/

---
+++ npms/epii-boot/dist-publish/bin/epii-boot-run.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const epii_boot_1 = require("epii-boot");
+(0, epii_boot_1.bootStart)();
---
+++ npms/epii-boot/dist-publish/src/tools.js
@@ -0,0 +1,27 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sort = void 0;
+function sort(packageMap) {
+    let out = new Set();
+    let xianghuset = new Set();
+    let addByKeys = function (keys) {
+        keys.forEach(key => {
+            packageMap[key].dependencies.forEach(ykey => {
+                if (xianghuset.has(key + "###" + ykey)) {
+                    throw new Error(key + " and " + ykey + " xun huan yinyong");
+                }
+                else {
+                    xianghuset.add(key + "###" + ykey);
+                    xianghuset.add(ykey + "###" + key);
+                }
+            });
+            addByKeys(packageMap[key].dependencies);
+            out.add(packageMap[key]);
+        });
+    };
+    Object.values(packageMap).forEach(item => {
+        addByKeys([item.name]);
+    });
+    return Array.from(out);
+}
+exports.sort = sort;
